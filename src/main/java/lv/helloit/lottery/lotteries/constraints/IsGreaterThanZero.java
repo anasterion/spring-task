af8e97bb96d262e34fc03fb7482ad369a25784bd
Viktor <vitjok102@gmail.com>
2019-01-06 15:28:31 +0200

    Finished validation logic for lottery registration

---
+++ src/main/java/lv/helloit/lottery/lotteries/constraints/IsGreaterThanZero.java
@@ -0,0 +1,20 @@
+package lv.helloit.lottery.lotteries.constraints;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = IsGreaterThanZeroConstraintValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD } )
+@Retention(RetentionPolicy.RUNTIME)
+public @interface IsGreaterThanZero {
+
+    String message() default "value should be number greater than zero";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}
